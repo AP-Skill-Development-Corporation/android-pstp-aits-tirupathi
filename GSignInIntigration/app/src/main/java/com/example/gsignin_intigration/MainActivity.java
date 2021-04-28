15759729df6fb56ba44114f535ff12b59dbe4c26
muneiah tellakula <muneiahtellakula@gmail.com>
2021-04-28 16:29:30 +0530

    google signin doc

--- GSignInIntigration/app/src/main/java/com/example/gsignin_intigration/MainActivity.java
+++ GSignInIntigration/app/src/main/java/com/example/gsignin_intigration/MainActivity.java
@@ -1,14 +1,132 @@
 package com.example.gsignin_intigration;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,View.OnClickListener {
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = "MainActivity";
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton signInButton;
+    private TextView mtextStatus, mEmail;
+    private ImageView profilePic;
+    private LinearLayout mLinearLayout;
+    private Button sign_out;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sign_out = (Button) findViewById(R.id.sign_Out_button);
+        mtextStatus = (TextView) findViewById(R.id.displayName);
+        mEmail = (TextView) findViewById(R.id.email);
+        profilePic = (ImageView) findViewById(R.id.displayPic);
+        mLinearLayout = (LinearLayout) findViewById(R.id.detailsLayout);
+        mLinearLayout.setVisibility(View.GONE);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Set the dimensions of the sign-in button.
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        sign_out.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_Out_button:
+                signOut();
+                break;
+            // ...
+        }
+        
+
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        // Write whatever you like to notify the user
+                        mLinearLayout.setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(), "You have been Logged out Sccessfully ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "connection faild", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result)
+    {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+
+            // Signed in successfully, show authenticated UI.
+            mLinearLayout.setVisibility(View.VISIBLE);
+            GoogleSignInAccount acct = result.getSignInAccount();
+            String email = acct.getEmail().toString();
+            mtextStatus.setText(""+acct.getDisplayName());
+            mEmail.setText(""+email);
+            Uri pic = acct.getPhotoUrl();
+            Glide.with(this).load(pic).into(profilePic);
+
+        } else {
+            // Signed out, show unauthenticated UI.
+            Toast.makeText(this, "sign out", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
